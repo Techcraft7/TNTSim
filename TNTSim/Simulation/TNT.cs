6a889d8d9a8afe408c93e5747b77e347fea5e3da
Techcraft7 <mctechcraft7@gmail.com>
2023-09-12 00:43:21 -0500

    reverted explosion math added loaded flag (see #1)

--- TNTSim/Simulation/TNT.cs
+++ TNTSim/Simulation/TNT.cs
@@ -7,10 +7,12 @@ internal struct TNT
     private const double CENTER_OFFSET = 0.98F * 0.0625D;
     private static uint NEXT_ID = 0;
 
+
     public bool Removed { get; private set; } = false;
     public readonly uint id;
     public Vec3 position, velocity;
     public int fuse;
+    public bool loaded;
 
     public TNT()
     {
@@ -20,9 +22,23 @@ internal struct TNT
 
     public void Tick(SimulationContext context, bool firstTick = false)
     {
+        loaded = true;
         velocity.Y -= 0.04;
 
+        if (firstTick)
+        {
+            velocity.X *= 0.9;
+            velocity.Y *= 1.5;
+            velocity.Z *= 0.9;
+        }
+
         position += velocity;
+
+        if (firstTick)
+        {
+            velocity = default;
+        }
+
         velocity *= 0.98;
 
         // If on ground
@@ -47,36 +63,39 @@ internal struct TNT
 
     public readonly void ExplodeOnto(ref TNT other)
     {
-        if (other.id == id)
+        if (other.id == id || !other.loaded)
         {
             return;
         }
 
-        // This is D_e and D_f because everything is TNT
-        double dx = other.position.X - position.X;
-        double dy = other.position.Y - position.Y;
-        double dz = other.position.Z - position.Z;
-        double squareDistance = (dx * dx) + (dy * dy) + (dz * dz);
-
-        if (squareDistance is 0 or >= 64)
+        Vec3 center = position + new Vec3(0, CENTER_OFFSET, 0);
+        double distanceNormalized = other.position.DistanceTo(center) / 8.0;
+        if (distanceNormalized > 1)
         {
             return;
         }
 
-        // Same as d = d - dy^2 + (dy + offset)^2 
-        squareDistance += (2 * dy * CENTER_OFFSET) + (CENTER_OFFSET * CENTER_OFFSET);
-        double l = Math.Sqrt(squareDistance);
-        double lInv = 1 / l;
-        double k = lInv - 0.125; // lInv * (1 - (l / 8)) = lInv - 1/8
-        dx *= k;
-        dy *= k;
-        dz *= k;
-        other.velocity = new(other.velocity.X + dx, other.velocity.Y + dy, other.velocity.Z + dz);
+        Vec3 dir = other.position - center;
+
+        double length = dir.Length();
+        if (length == 0)
+        {
+            return;
+        }
+
+        dir /= length;
+
+        const double EXPOSURE = 1.0;
+        double magnitude = (1 - distanceNormalized) * EXPOSURE;
+
+        dir *= magnitude;
+
+        other.velocity += dir;
     }
 
     public static bool operator ==(TNT a, TNT b) => a.id == b.id;
     public static bool operator !=(TNT a, TNT b) => a.id != b.id;
 
-	public override readonly bool Equals([NotNullWhen(true)] object? obj) => obj is TNT other && this == other;
-	public override readonly int GetHashCode() => id.GetHashCode();
+    public override readonly bool Equals([NotNullWhen(true)] object? obj) => obj is TNT other && this == other;
+    public override readonly int GetHashCode() => id.GetHashCode();
 }
