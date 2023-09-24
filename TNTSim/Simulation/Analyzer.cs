4b466b863648395465a143a56a98431f85b3776c
Techcraft7 <mctechcraft7@gmail.com>
2023-09-24 16:16:30 -0500

    add analyzer class

---
+++ TNTSim/Simulation/Analyzer.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+namespace TNTSim.Simulation;
+
+internal class Analyzer
+{
+    public double PercentComplete => currentTick / 80.0 * 100.0;
+    public IReadOnlyList<double> MSPT => mspt;
+    public IReadOnlyList<int> TNTCounts => tntCounts;
+
+    private readonly SimulationContext context;
+    private readonly Task task;
+    private readonly double[] mspt = new double[80];
+    private readonly int[] tntCounts = new int[80];
+    private int currentTick = 0;
+
+    public Analyzer(SimulationSettings settings)
+    {
+        context = Simulator.Create(settings);
+        task = Task.Run(Analyze);
+    }
+
+    private void Analyze()
+    {
+        // Measure TNT counts and MSPT
+        Stopwatch sw = new();
+        TimeSpan last = TimeSpan.Zero;
+        while (currentTick < 80)
+        {
+            tntCounts[currentTick] = context.TNT.Count;
+            
+            sw.Restart();
+            context.Tick();
+            
+            TimeSpan elapsed = sw.Elapsed;
+            mspt[currentTick] = (elapsed - last).TotalMilliseconds;
+            last = elapsed;
+
+            currentTick++;
+        }
+
+        // Analyze explosions
+        // TODO
+    }
+
+    public bool IsComplete() => task.IsCompleted;
+}
