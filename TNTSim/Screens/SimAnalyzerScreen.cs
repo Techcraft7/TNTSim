87ef99036d7f0889639ec17dffc44f9b423ec8b0
Techcraft7 <mctechcraft7@gmail.com>
2023-09-24 16:28:20 -0500

    add basic analyzer screen

---
+++ TNTSim/Screens/SimAnalyzerScreen.cs
@@ -0,0 +1,22 @@
+﻿namespace TNTSim.Screens;
+
+internal static class SimAnalyzerScreen
+{
+    private static Analyzer? current = null;
+
+    public static bool UpdateAndDraw()
+    {
+        if (current is null || IsKeyPressed(KeyboardKey.KEY_ESCAPE) || IsKeyPressed(KeyboardKey.KEY_TAB))
+        {
+            current = null;
+            return false;
+        }
+
+        DrawText("TODO", 100, 100, FONT_SIZE, Color.RED);
+        DrawText($"{current.PercentComplete:F2}%", 100, 100 + FONT_SIZE + PADDING, FONT_SIZE, Color.RED);
+
+        return true;
+    }
+
+    public static void Start(SimulationSettings settings) => current = new(settings);
+}
--- TNTSim/Screens/SimulationScreen.cs
+++ TNTSim/Screens/SimulationScreen.cs
@@ -8,8 +8,8 @@ internal static class SimulationScreen
     private const string TITLE = "Simulator";
     private static readonly int TITLE_X = (WINDOW_WIDTH - MeasureText(TITLE, FONT_SIZE)) / 2;
     private static readonly ComponentGroup SETTINGS = new ComponentGroupBuilder()
-        .AddButton("KABOOM!", onClick: () => shouldStart = true)
-        .AddButton("Analyze", onClick: () => { })
+        .AddButton("KABOOM!", onClick: () => SetSubscreen(Subscreen.PREVIEW))
+        .AddButton("Analyze", onClick: () => SetSubscreen(Subscreen.ANALYZER))
         .EndRow()
         .AddText("Payload Y")
         .AddNumberBox(0, 319, initial: 255)
@@ -20,36 +20,66 @@ internal static class SimulationScreen
 		.EndRow()
 		.Build(START_Y);
 
-    private static bool shouldStart = false;
-    private static bool started = false;
+    private static Subscreen subscreen = Subscreen.MAIN;
+    private static bool first = false;
 
     public static void UpdateAndDraw(ref CannonSettings cannonSettings)
     {
-        if (started)
-        {
-            started = SimPreviewScreen.UpdateAndDraw();
-        }
-        else
-        {
-            SETTINGS.UpdateAndDraw();
-        }
-
-        DrawRectangle(0, 0, WINDOW_WIDTH, CONTROL_HEIGHT + PADDING, Color.WHITE);
+        DrawRectangle(0, 0, WINDOW_WIDTH, CONTROL_HEIGHT + PADDING, Color.RAYWHITE);
         DrawText(TITLE, TITLE_X, 2 * PADDING, FONT_SIZE, Color.GRAY);
         DrawLine(0, CONTROL_HEIGHT + PADDING, WINDOW_WIDTH, CONTROL_HEIGHT + PADDING, Color.GRAY);
 
-        if (shouldStart)
+        switch (subscreen)
         {
-			SimulationSettings settings = new()
-			{
-				cannonSettings = cannonSettings,
-				payloadY = SETTINGS.GetComponent<NumberBox>(3).Value,
-				evenSpacing = SETTINGS.GetComponent<CheckBox>(4).Value,
-				showVelocity = SETTINGS.GetComponent<CheckBox>(5).Value,
-			};
-			SimPreviewScreen.Start(settings);
-            shouldStart = false;
-            started = true;
+            case Subscreen.MAIN:
+                SETTINGS.UpdateAndDraw();
+                break;
+            case Subscreen.PREVIEW:
+                if (first)
+                {
+                    SimulationSettings settings = GetSimSettings(cannonSettings);
+                    SimPreviewScreen.Start(settings);
+                    first = false;
+                }
+                else if (!SimPreviewScreen.UpdateAndDraw())
+                {
+                    subscreen = Subscreen.MAIN;
+                }
+                break;
+            case Subscreen.ANALYZER:
+                if (first)
+                {
+                    SimulationSettings settings = GetSimSettings(cannonSettings);
+                    SimAnalyzerScreen.Start(settings);
+                    first = false;
+                }
+                else if (!SimAnalyzerScreen.UpdateAndDraw())
+                {
+                    subscreen = Subscreen.MAIN;
+                }
+                break;
         }
+
+    }
+
+    private static SimulationSettings GetSimSettings(CannonSettings cannonSettings) => new()
+    {
+        cannonSettings = cannonSettings,
+        payloadY = SETTINGS.GetComponent<NumberBox>(3).Value,
+        evenSpacing = SETTINGS.GetComponent<CheckBox>(4).Value,
+        showVelocity = SETTINGS.GetComponent<CheckBox>(5).Value,
+    };
+
+    private static void SetSubscreen(Subscreen scr)
+    {
+        subscreen = scr;
+        first = true;
+    }
+
+    private enum Subscreen
+    {
+        MAIN,
+        PREVIEW,
+        ANALYZER
     }
 }
