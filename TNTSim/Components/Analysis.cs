10fa2e537cca397e58c5288ab40a8948333f21e7
Techcraft7 <mctechcraft7@gmail.com>
2023-09-28 22:17:17 -0500

    add analysis text

---
+++ TNTSim/Components/Analysis.cs
@@ -0,0 +1,22 @@
+﻿namespace TNTSim.Components;
+internal sealed class Analysis : Component
+{
+    public const int HEIGHT = 200;
+    public const int TEXT_Y = PADDING;
+
+    public IReadOnlyList<double> Data { get; set; } = Array.Empty<double>();
+
+    private readonly string name;
+    private readonly int textX;
+
+    public Analysis(string name, int y) : base(PADDING, y, WINDOW_WIDTH - (PADDING * 2), HEIGHT)
+    {
+        this.name = name;
+        textX = X + ((Width - MeasureText(name, FONT_SIZE)) / 2);
+    }
+
+    public override void UpdateAndDraw()
+    {
+        DrawText(name, textX, Y + TEXT_Y, FONT_SIZE, PrimaryColor);
+    }
+}
--- TNTSim/Screens/SimAnalyzerScreen.cs
+++ TNTSim/Screens/SimAnalyzerScreen.cs
@@ -3,9 +3,16 @@
 internal static class SimAnalyzerScreen
 {
     private const int START_Y = CONTROL_HEIGHT + PADDING + PADDING;
+    private const int PROGRESS_Y = START_Y + CONTROL_HEIGHT;
+    private const int TNT_GRAPH_Y = START_Y + Analysis.HEIGHT + PADDING;
     private const string LOADING_TEXT = "Loading...";
+    
     private static readonly int LOADING_TEXT_X = (WINDOW_WIDTH - MeasureText(LOADING_TEXT, FONT_SIZE)) / 2;
+    private static readonly Analysis MSPT_GRAPH = new("MSPT", START_Y);
+    private static readonly Analysis TNT_GRAPH = new("TNT", TNT_GRAPH_Y);
+    
     private static Analyzer? current = null;
+    private static bool firstAfterDone = true;
 
     public static bool UpdateAndDraw()
     {
@@ -18,11 +25,23 @@ internal static class SimAnalyzerScreen
         if (!current.IsComplete())
         {
             DrawText(LOADING_TEXT, LOADING_TEXT_X, START_Y, FONT_SIZE, Color.GRAY);
-            DrawRectangleLines(PADDING, START_Y + CONTROL_HEIGHT, WINDOW_WIDTH - (2 * PADDING), FONT_SIZE, Color.GRAY);
+            
+            DrawRectangleLines(PADDING, PROGRESS_Y, WINDOW_WIDTH - (2 * PADDING), FONT_SIZE, Color.GRAY);
             int w = (int)(current.PercentComplete * (WINDOW_WIDTH - (4 * PADDING)));
-            DrawRectangle(PADDING * 2, START_Y + CONTROL_HEIGHT + PADDING, w, FONT_SIZE - (2 * PADDING), Color.GREEN);
+            DrawRectangle(PADDING * 2, PROGRESS_Y + PADDING, w, FONT_SIZE - (2 * PADDING), Color.GREEN);
+            firstAfterDone = true;
             return true;
         }
+
+        if (firstAfterDone)
+        {
+            MSPT_GRAPH.Data = current.MSPT;
+            TNT_GRAPH.Data = current.TNTCounts.Select(static i => (double)i).ToList();
+            firstAfterDone = false;
+        }
+
+        MSPT_GRAPH.UpdateAndDraw();
+        TNT_GRAPH.UpdateAndDraw();
 
         return true;
     }
