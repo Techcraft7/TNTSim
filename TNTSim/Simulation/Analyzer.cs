608394444a819012d252a4282df8aebf1b36d737
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-11-30 19:26:25 -0600

    move 2.0 features to branch

--- TNTSim/Simulation/Analyzer.cs
+++ TNTSim/Simulation/Analyzer.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Security.Cryptography;
 
 namespace TNTSim.Simulation;
 
@@ -8,11 +7,6 @@ internal class Analyzer
 	public double PercentComplete => currentTick / 80.0;
 	public IReadOnlyList<double> MSPT => mspt;
 	public IReadOnlyList<int> TNTCounts => tntCounts;
-	public double CenterX { get; private set; } = double.NaN;
-	public double CenterZ { get; private set; } = double.NaN;
-	public double DamageRadiusX { get; private set; } = double.NaN;
-	public double DamageRadiusZ { get; private set; } = double.NaN;
-	public double DamagePercentage { get; private set; } = double.NaN;
 
 	private readonly Simulation context;
 	private readonly Task task;
@@ -29,35 +23,6 @@ internal class Analyzer
 	private void Analyze()
 	{
 		MeasureMSPTAndTNTCount();
-
-		CalculateExplosionMetrics();
-	}
-
-	private void CalculateExplosionMetrics()
-	{
-		CenterX = CenterZ = DamageRadiusX = DamageRadiusZ = DamagePercentage = double.NaN;
-		if (context.Explosions.Count == 0)
-		{
-			return;
-		}
-
-		List<Vec3> onGround = context.Explosions.Where(static v => Math.Abs(v.Y) <= EXPLOSION_SIZE).ToList();
-
-		double minX = onGround.Min(static v => v.X);
-		double minZ = onGround.Min(static v => v.Z);
-		double maxX = onGround.Max(static v => v.X);
-		double maxZ = onGround.Max(static v => v.Z);
-
-		DamageRadiusX = (maxX - minX) / 2.0;
-		DamageRadiusZ = (maxZ - minZ) / 2.0;
-
-		CenterX = (minX + maxX) / 2.0;
-		CenterZ = (minZ + maxZ) / 2.0;
-
-		// TODO: account for overlapping
-		double damagedArea = 0;
-		double totalArea = Math.PI * DamageRadiusX * DamageRadiusZ;
-		DamagePercentage = damagedArea / totalArea;
 	}
 
 	private void MeasureMSPTAndTNTCount()
