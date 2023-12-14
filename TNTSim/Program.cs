797a388a5067b23043fd0efc6b5ce82b3a130586
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-12-13 20:28:55 -0500

    add settings file

--- TNTSim/Program.cs
+++ TNTSim/Program.cs
@@ -2,6 +2,8 @@
 
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.Configuration.Sources.Clear();
+builder.Configuration.AddJsonFile("settings.json");
 builder.Services.AddHostedService<RendererService>();
 
 var app = builder.Build();
