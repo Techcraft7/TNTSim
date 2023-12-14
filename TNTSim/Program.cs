9a5aa74f54d78f2286f4cd94711285de73e272e4
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-12-13 19:57:34 -0500

    begin new UI

---
+++ TNTSim/Program.cs
@@ -0,0 +1,5 @@
+﻿var builder = Host.CreateApplicationBuilder(args);
+
+var app = builder.Build();
+
+app.Run();
---
+++ TNTSim/Renderer.cs
@@ -0,0 +1,22 @@
+﻿using static Raylib_cs.Raylib;
+
+namespace TNTSim;
+
+internal sealed class Renderer : BackgroundService
+{
+	protected override Task ExecuteAsync(CancellationToken stoppingToken)
+	{
+		InitWindow(800, 600, "TNT Cannon Simulator");
+
+		while (!WindowShouldClose() && !stoppingToken.IsCancellationRequested)
+		{
+			BeginDrawing();
+
+			EndDrawing();
+		}
+
+		CloseWindow();
+
+		return Task.CompletedTask;
+	}
+}
