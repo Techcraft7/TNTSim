fdd9e85c7c6b61a5fdf4d53bdb7c32c90ba0c4c2
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-12-13 19:49:41 -0500

    NEW UI TIME WOOOOOOOOOOO

--- TNTSim/ICloneable.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace TNTSim;
-
-internal interface ICloneable<out T> where T : ICloneable<T>
-{
-	public T Clone();
-}
--- TNTSim/Simulation/Settings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TNTSim.Simulation;
-
-internal struct Settings
-{
-	public SimulatorSettings simulatorSettings;
-	public CannonSettings cannonSettings;
-	public bool evenSpacing;
-	public bool showVelocity;
-}
