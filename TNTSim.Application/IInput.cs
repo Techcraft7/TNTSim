13138e4cf6cc3ba3f16bbd8142800df6730b4cf7
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2023-12-16 16:48:16 -0500

    create iinput

---
+++ TNTSim.Application/IInput.cs
@@ -0,0 +1,18 @@
+﻿namespace TNTSim.Application;
+
+public interface IInput
+{
+	public bool IsKeyDown(char c);
+	public bool IsKeyPressed(char c);
+	public bool IsKeyReleased(char c);
+
+	public bool IsLeftMouseDown();
+	public bool IsLeftMousePressed();
+	public bool IsLeftMouseReleased();
+
+	public bool IsRightMouseDown();
+	public bool IsRightMousePressed();
+	public bool IsRightMouseReleased();
+
+	public int GetScrollWheel();
+}
